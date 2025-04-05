9fc68f95af61319d4cf160af49c5835fd9be44cc
kkgobkk <79253828+kkgobkk@users.noreply.github.com>
2025-04-05 18:57:12 +0200

    Update ImmersiveTravel.cs
    basic road integration, better debug messages

--- Scripts/ImmersiveTravel.cs
+++ Scripts/ImmersiveTravel.cs
@@ -1,4 +1,4 @@
-﻿//this is the main class of the mod. It registers all new factions and guild services (CarriageDriversGuild.cs)  and the overridden map (ImmersiveTravelMap.cs) to the game.
+//this is the main class of the mod. It registers all new factions and guild services (CarriageDriversGuild.cs)  and the overridden map (ImmersiveTravelMap.cs) to the game.
 
 using System;
 using System.Collections.Generic;
@@ -25,16 +25,33 @@ namespace ImmersiveTravel{
     {
         static public Mod mod;
         static private StaticNPC npc;
+        static public bool BasicRoadsEnabled { get; private set; }
+        
+        //direction constants copied from BasicRoadsTexturing
+        public const byte N = 128; //0b_1000_0000;
+        public const byte NE = 64; //0b_0100_0000;
+        public const byte E = 32;  //0b_0010_0000;
+        public const byte SE = 16; //0b_0001_0000;
+        public const byte S = 8;   //0b_0000_1000;
+        public const byte SW = 4;  //0b_0000_0100;
+        public const byte W = 2;   //0b_0000_0010;
+        public const byte NW = 1;  //0b_0000_0001;
 
         [Invoke(StateManager.StateTypes.Start, 0)]
         public static void Init(InitParams initParams){
+            Debug.Log("init mod: ImmersiveTravel");
             mod = initParams.Mod;
             if(!(AddCarriageDriversFaction() && AddTravelService() && AddFakeGuild()))
                 throw new Exception("faction and service ID is already used.");
+            Mod BasicRoads = ModManager.Instance.GetMod("BasicRoads");
+            BasicRoadsEnabled = (BasicRoads != null) && BasicRoads.Enabled;
+            if(BasicRoads != null)
+                Debug.Log("ImmerisveTravel: basic roads mod found");
             if(mod.GetSettings().GetValue<bool>("General", "DisableFastTravel"))
                 UIWindowFactory.RegisterCustomUIWindow(UIWindowType.TravelMap, typeof(ImmersiveTravelMap));
             UIWindowFactory.RegisterCustomUIWindow(UIWindowType.TravelPopUp, typeof(ImmersiveTravelPopUp));
             ModManager.Instance.GetMod(initParams.ModTitle).IsReady = true;
+            Debug.Log("ImmersiveTravel: init finished");
         }
 
         //when loading the mod, replaces the vanilla travel map with a disabled one (you can open it, but not travel)
